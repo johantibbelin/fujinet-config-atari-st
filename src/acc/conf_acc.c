e738a3387fe36d949727f8147af7c8fb7f13fd70
Johan Tibbelin <johan.roos@tibbelin.com>
2022-06-23 12:15:30 +0200

    Start of FujiNet config ACC

--- src/acc/conf_acc.c
+++ src/acc/conf_acc.c
@@ -1,10 +1,52 @@
 /**
 * Fujinet config desk ACC for the Atari ST
 */
-//#include <osbind.h>
+#include <osbind.h>
+#include <gem.h>
 #include <stdio.h>
-int main()
+
+/**
+ * Helper functions 
+ */
+int close_app() {return 0;}
+int init_app()
 {
-  printf("Hello, World!\n");
+
+  int prog_handle = appl_init();
+
+  if (prog_handle != 0)
+    (void)  menu_register(prog_handle,"  FujiNet config");
+  else
+    return -1;
+  
   return 0;
 }
+int main_program()
+{
+
+  short evt_buff[16];
+  while (1) 
+    {
+      (void) evnt_mesag(evt_buff);
+
+      // check if ACC menu item is clicked
+      if (evt_buff[0] == 40)
+	{
+	  // Do stuff
+	  printf("Doing stuff here...\n");
+	}
+    }
+  
+}
+int main()
+{
+  
+  if (init_app() == -1)
+    goto close;
+  main_program();
+  close:
+  close_app();
+ 
+  return 0; 
+}
+ 
